906b63371323419773c7388467c0396096c7458d
RennamFariaUNIFESP <143340352+RennamFariaUNIFESP@users.noreply.github.com>
2023-09-20 14:02:46 -0300

    Update user.ts

--- Ex7/user.ts
+++ Ex7/user.ts
@@ -2,7 +2,7 @@ export class User {
     constructor(
         public name: string,
         public email: string,
-        public password: string,    //salva como Hash
+        public password: string,    //salva como Hash 
         public id?: string
     ) {}
 }
