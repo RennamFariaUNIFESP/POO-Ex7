1193a4c5ada65a370effab067d24f159c8b2cdbb
RennamFariaUNIFESP <143340352+RennamFariaUNIFESP@users.noreply.github.com>
2023-09-20 14:01:03 -0300

    Update app.ts

--- Ex7/app.ts
+++ Ex7/app.ts
@@ -104,7 +104,7 @@ export class App {
 }
 
 function diffHours(dt2: Date, dt1: Date) {
-  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  var diff = (dt2.getTime() - dt1.getTime()) / 1000
   diff /= (60 * 60);
   return Math.abs(diff);
 }
